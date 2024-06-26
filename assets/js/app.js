2cb20500a398b92f51657ae7b715e616226eaca3
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-06-26 19:09:34 +0800

    initial main.js and header functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,46 @@
  * @copyright matuladaandi 2024
  */
 
+"use strict";
 
-'use strict';
+/**
+ * Add eventlistener on multiple elements
+ * @param {NodeList} $elements
+ * @param {String} eventType String
+ * @param {Function} callback Function
+ */
 
+const addEventOnElements = function ($elements, eventType, callback) {
+  for (const $item of $elements) {
+    $item.addEventListener(eventType, callback);
+  }
+};
+
+/**
+ * Header scroll state
+ */
+
+const /**{NodeElement} */ header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Search toggle
+ */
+
+const /**{NodeElement} */ searchToggler = document.querySelector(
+    "[data-search-toggle]"
+  );
+const /**{NodeELement} */ searchField = document.querySelector(
+    "[data-search-field]"
+  );
+let /** {Boolean} */ isExpanded = false;
+
+searchToggler.addEventListener("click", function () {
+  header.classList.toggle("search-active");
+  isExpanded = isExpanded ? false : true;
+  this.setAttribute("aria-expanded", isExpanded);
+  searchField.focus();
+});
