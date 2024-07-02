e9bb137b34c87c01b3b2b4d27dc7111bec39a293
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-07-02 18:54:40 +0800

    initial api.js file

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,34 @@
 /**
- * @license MIT
- * @author codewithsadee <mohammadsadee24@gmail.com>
- * @copyright codewithsadee 2023
+ * @author matuladaandi <matuladaandi@gmail.com>
+ * @copyright matuladaandi 2024
  */
 
+"use strict";
 
-'use strict';
+/**
+ * Fecth data from server
+ * @param {*} url API Url [required]
+ * @param {*} successCallback Success callback [required]
+ * @param {*} errorCallback Error callback [optional]
+ */
+
+// Mendefinisikan fungsi asinkron bernama fetchData yang diekspor untuk digunakan di modul lain
+export async function fetchData(url, successCallback, errorCallback) {
+  // Melakukan permintaan HTTP ke URL yang diberikan menggunakan fetch
+  const responese = await fetch(url);
+
+  // Memeriksa apakah permintaan berhasil dengan memeriksa properti ok dari responese
+  if (responese.ok) {
+    // mengambil data yang dikirim oleh server dalam format JSON dan mengubahnya menjadi objek JavaScript yang dapat digunakan
+    const data = await responese.json();
+
+    // Memanggil callback sukses dengan data yang diambil
+    successCallback(data);
+  } else {
+    // Mengurai respons JSON jika permintaan gagal
+    const error = await responese.json();
+    
+    // Memanggil callback kesalahan jika didefinisikan
+    errorCallback && errorCallback(error);
+  }
+}
